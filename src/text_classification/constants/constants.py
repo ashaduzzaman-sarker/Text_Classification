73ac578cbfe26129c4bde3f52356682989ceac73
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2025-11-04 14:12:40 +0000

    constants

---
+++ src/text_classification/constants/constants.py
@@ -0,0 +1,7 @@
+# ============================================================================
+# src/text_classification/constants/constants.py
+# ============================================================================
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("config/params.yaml")
